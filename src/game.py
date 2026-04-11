81d74f9efceaf540e67ff965bd12c859bec514f3
Armando Barragan <armandobp765@gmail.com>
2026-04-11 17:24:41 -0600

    Created start quest method (empty)

---
+++ src/game.py
@@ -0,0 +1,8 @@
+from random import random
+class Game:
+    def __init__(self, character):
+        self.character = character
+        self.quests = []
+    
+    def start_quest(self):
+        pass # This method starts a quest, creating three random encounters
