296b6db2974012f5bf5e47c41a235cbe4c813561
derekterra <derekterra@hotmail.com>
2026-06-28 20:42:20 -0600

    feat: add character persistence on db during game creation flow

--- app/src/repositories/__init__.py
+++ app/src/repositories/__init__.py
@@ -1,5 +1,6 @@
 from .quest_repository import QuestRepository
 from .game_repository import GameRepository
 from .user_repository import UserRepository
+from .character_repository import CharacterRepository
 
-__all__ = ["QuestRepository", "GameRepository", "UserRepository"]
+__all__ = ["QuestRepository", "GameRepository", "UserRepository", "CharacterRepository"]
--- app/src/services/game_service.py
+++ app/src/services/game_service.py
@@ -1,5 +1,5 @@
 from src.game import Game
-from src.repositories import GameRepository, QuestRepository
+from src.repositories import GameRepository, QuestRepository, CharacterRepository
 from src.game_engine.scene import Scene
 from src.game_engine.quest import Quest
 from src.models.quest_model import QuestStatus
@@ -9,9 +9,11 @@ class GameService:
         self,
         game_repository: GameRepository,
         quest_repository: QuestRepository,
+        character_repository: CharacterRepository,
     ):
         self.game_repository = game_repository
         self.quest_repository = quest_repository
+        self.character_repository = character_repository
 
     def create_game(self, user_id: int) -> tuple[Game, int, int]:
         game = Game()
@@ -20,6 +22,8 @@ class GameService:
         db_quest = self.quest_repository.create_quest(
             db_game.id, quest.story_key, quest.name, quest.description
         )
+        character = game.character
+        db_character = self.character_repository.create_character(user_id,db_game.id, character)
         db_game.active_quest_id = db_quest.id
         self.game_repository.update_game(db_game)
         return game, db_game.id, db_quest.id
@@ -61,3 +65,4 @@ class GameService:
                 quest_record.current_act_index
             ].scenes[quest_record.current_scene_index]
         )
+    
